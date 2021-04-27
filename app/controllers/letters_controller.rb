30b77fd5110390f995c20db892c2e91a4fc9763f
Yukimarupie <y.harada59@gmail.com>
2021-04-27 12:37:36 +0900

    edit some views

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,10 +1,26 @@
 class LettersController < ApplicationController
   def index
+    @letters = Letter.all
   end
 
   def show
+    @letter = Letter.find(params[:id])
   end
 
   def new
+    @letter = Letter.new(letter_params)
   end
+
+  def create
+    letter = Letter.new(letter_params)
+    letter.save!
+    redirect_to letters_url, notice: "手紙が送信されました"
+  end
+
+  private
+
+  def letter_params
+    params.require(:letter).permit(:discription, :send_by, :send_to)
+  end
+
 end
