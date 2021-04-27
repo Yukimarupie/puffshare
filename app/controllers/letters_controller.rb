7e20e785dbaafccc1098f00730e538e5ce722b51
Yukimarupie <y.harada59@gmail.com>
2021-04-27 14:46:22 +0900

    display flash message and implement letter_show

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -3,10 +3,6 @@ class LettersController < ApplicationController
     @letters = Letter.all
   end
 
-  def show
-    @letter = Letter.find(params[:id])
-  end
-
   def new
     @letter = Letter.new
   end
@@ -14,13 +10,19 @@ class LettersController < ApplicationController
   def create
     letter = Letter.new(letter_params)
     letter.save!
-    redirect_to letters_url, notice: "手紙が送信されました"
+    redirect_to letters_path, notice: "手紙が送信されました"
   end
+
+  def show
+    @letter = Letter.find(params[:id])
+  end
+
+
 
   private
 
   def letter_params
-    params.require(:letter).permit(:description, :send_by, :send_to)
+    params.require(:letter).permit(:description, :send_by, :send_to, :api_status)
   end
 
 end
