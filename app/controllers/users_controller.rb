93ad521113c04348671d157804d902257a650eee
Yukimarupie <y.harada59@gmail.com>
2021-04-29 16:53:06 +0900

    delete some unnecessary constant in login.js and add JS path in new.html.erb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   
   def create
     idToken = params[:idToken]
-    channelId = ENV['LINE_CHANNEL_IF']
+    channelId = ENV['LINE_CHANNEL_ID']
     res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'),
                           {'id_token'=>idToken, 'client_id'=>channelId})
     render :json => res.body
