7f088151c8a0ce72301f6d131d55547c81e2566f
Yukimarupie <y.harada59@gmail.com>
2021-05-04 09:17:30 +0900

    delete unneccesary code in config/env.js

---
+++ app/javascript/packs/letters/targetpicker.js
@@ -0,0 +1,7 @@
+// 参考: https://qiita.com/chuck0523/items/1868a4c04ab4d8cdfb23 cmd+F=CommonJS
+//　別ファイルで呼び出したい時はこんな感じ？？↓　
+
+// const myModule = require('loginModule');
+
+// const loginModuleInstance = new loginModule();
+// console.log(loginModuleInstance);
