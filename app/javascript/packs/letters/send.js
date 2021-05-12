e77903af92d76935231c8de32bdcad91fcd97f4e
Yukimarupie <y.harada59@gmail.com>
2021-05-12 12:49:17 +0900

    EXPLANATION about JS

--- app/javascript/packs/letters/send.js
+++ app/javascript/packs/letters/send.js
@@ -1,16 +1,19 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', () => {
   // 他のメソッドを実行できるよう初期化
   liff.init({
-    liffId: '1655861824-xLoVRAkl'
+    liffId: '1655861824-xLoVRAkl' //process.env.LIFF_ID
   })
     // ログイン処理
     .then(() => {
       if (!liff.isLoggedIn()) {
         liff.login();
-      } else {
+      }
+      else {
         const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
         const idToken = liff.getIDToken()
-        const body = `idToken=${idToken}` //ここでbodyにidTokenを入れ込んで
+        const bodyInIdToken = `idToken=${idToken}` //ここでbodyにidTokenを入れ込んで
         // console.log(body)
         const request = new Request('/users', {
           headers: {
@@ -18,12 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
             'X-CSRF-Token': token
           },
           method: 'POST',
-          body: body //ここで格納してRailsのusersコントローラに投げる
+          body: bodyInIdToken //ここで格納してRailsのusersコントローラに投げる
         });
         // console.log(request);
         // liff.getIDToken()で取得したIDTokenの情報をfetchメソッドを使ってRailsに渡す
         fetch(request)
-          .then(response => response.json())
+          .then(response => response.json())//これはfetchの鉄板文。このJSON変換？処理を挟まないと、 Promiseで返ってきたresponseの中身がわからないが、これのおかげで何が入ってるか見れる。JSONは直接JSで使えないため、JSON.parse();する必要があるが、このresponse.json()では自動でパースできる
           .then(data => {
             console.log(data)
             //プロフィール情報を取得し、HTMLに投げる為の処理
@@ -43,21 +46,23 @@ document.addEventListener('DOMContentLoaded', () => {
               })
           })
       }
-
     })
-  document.getElementById('send').addEventListener('click', send);
-  function send() {
-    const redirectUrl = 'https://google.com'
+
+  const DomFormContents = document.querySelector('#form');
+  DomFormContents.addEventListener('ajax:success', (event) => {
+    console.log(event);
+    // ここでshared target pickerを呼び出す
+    const redirect_url = 'https://google.comhttps://liff.line.me/1655861824-xLoVRAkl/login?token=${event.detail[0].token}'
     if (liff.isApiAvailable('shareTargetPicker')) {
       liff.shareTargetPicker([
         message = {
           "type": "template",
           "altText": 'あなた宛のお手紙が届いてます！',
           "template": {
-            "thumbnailImageUrl": "https://user-images.githubusercontent.com/64563988/117566498-d6115880-b0f1-11eb-8fa2-236d6f05c6ae.png",
+            "thumbnailImageUrl": "https://user-images.githubusercontent.com/64563988/117742992-dff5a180-b240-11eb-894c-b94c7c2b2d47.png",
             "type": "buttons",
-            "title": "幸せの青い鳥からお手紙が届きました！",
-            "text": "早速読んでみよう〜",
+            "title": "幸せの青い鳥がお手紙を届けてくれたよ！",
+            "text": "さっそく読んでみよう〜",
             "actions": [
               {
                 "type": "uri",
@@ -67,7 +72,53 @@ document.addEventListener('DOMContentLoaded', () => {
             ]
           }
         }
-      ])
+      ]).then(() => {
+        liff.closeWindow();
+      })
+        .then(() => {
+          alert('お手紙が送信できました！')
+        })
+        .catch(() => {
+          alert("送信に失敗しました…")
+        });
     }
-  }
+  })
+
+
+
 })
+
+
+  // document.getElementById('send').addEventListener('click', send);
+  // function send() {
+  //   const redirectUrl = 'https://google.comhttps://liff.line.me/1655861824-xLoVRAkl/login?token=${event.detail[0].token}'
+  //   if (liff.isApiAvailable('shareTargetPicker')) {
+  //     liff.shareTargetPicker([
+  //       message = {
+  //         "type": "template",
+  //         "altText": 'あなた宛のお手紙が届いてます！',
+  //         "template": {
+  //           "thumbnailImageUrl": "https://user-images.githubusercontent.com/64563988/117566498-d6115880-b0f1-11eb-8fa2-236d6f05c6ae.png",
+  //           "type": "buttons",
+  //           "title": "幸せの青い鳥からお手紙が届きました！",
+  //           "text": "早速読んでみよう〜",
+  //           "actions": [
+  //             {
+  //               "type": "uri",
+  //               "label": "ここをクリックしてね☺︎",
+  //               "uri": redirectUrl
+  //             }
+  //           ]
+  //         }
+  //       }
+  //     ]).then(() => {
+  //       liff.closeWindow();
+  //     })
+  //       .then(() => {
+  //         alert('お手紙が送信できました！')
+  //       })
+  //       .catch(() => {
+  //         alert("送信に失敗しました…")
+  //       });
+  //   }
+  // }
